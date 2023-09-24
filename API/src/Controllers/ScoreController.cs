bea256af6c74357c4cf6a1404fc1e5db31b9ed31
Justwouter <wouter2312@gmail.com>
2023-09-24 16:01:40 +0200

    Restructured api order, Added unity compilation parameter

--- API/src/Controllers/ScoreController.cs
+++ API/src/Controllers/ScoreController.cs
@@ -8,109 +8,94 @@ using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Model;
 
-namespace API.Controllers
-{
+namespace API.Controllers {
     [Route("api/ScoreBoard")]
     [ApiController]
-    public class ScoreController : ControllerBase
-    {
+    public class ScoreController : ControllerBase {
         private readonly ScoreContext _context;
 
-        public ScoreController(ScoreContext context)
-        {
+        public ScoreController(ScoreContext context) {
             _context = context;
         }
 
-        // GET: api/Score
         [HttpGet("GetScores")]
-        public async Task<ActionResult<IEnumerable<Score>>> GetScores()
-        {
-          if (_context.Scores == null)
-          {
-              return NotFound();
-          }
+        public async Task<ActionResult<IEnumerable<Score>>> GetScores() {
+            if (_context.Scores == null) {
+                return NotFound();
+            }
             return await _context.Scores.ToListAsync();
         }
 
-        [HttpGet("GetScoreBoard")]
-        public async Task<ActionResult<IEnumerable<Score>>> GetScoreBoard()
-        {
-          if (_context.Scores == null)
-          {
-              return NotFound();
-          }
-            var allScores = await _context.Scores.ToListAsync();
-            return allScores.OrderBy(e => e.Highscore).Take(10).ToList();
-        }
-
-        [HttpGet("GetScoreBoardEntry/{id}")]
-        public async Task<ActionResult<Score>> GetScoreBoardEntry(int id)
-        {
-          if (_context.Scores == null)
-          {
-              return NotFound();
-          }
-            var allScores = await _context.Scores.ToListAsync();
-            return allScores.OrderBy(e => e.Highscore).Reverse().Take(10).ToArray()[id];
-        }
-
-        // GET: api/Score/5
         [HttpGet("GetScore/{id}")]
-        public async Task<ActionResult<Score>> GetScore(int id)
-        {
-          if (_context.Scores == null)
-          {
-              return NotFound();
-          }
+        public async Task<ActionResult<Score>> GetScore(int id) {
+            if (_context.Scores == null) {
+                return NotFound();
+            }
             var score = await _context.Scores.FindAsync(id);
 
-            if (score == null)
-            {
+            if (score == null) {
                 return NotFound();
             }
 
             return score;
         }
 
-        // POST: api/Score
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost("NewScoreForm")]
-        public async Task<ActionResult<Score>> PostScoreForm([FromForm] Score score)
-        {
-            Console.WriteLine(score.Highscore);
-          if (_context.Scores == null)
-          {
-              return Problem("Entity set 'ScoreContext.Scores'  is null.");
-          }
-            _context.Scores.Add(score);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetScore", new { id = score.Id }, score);
+
+        [HttpGet("GetScoreBoard")]
+        public async Task<ActionResult<IEnumerable<Score>>> GetScoreBoard() {
+            if (_context.Scores == null) {
+                return NotFound();
+            }
+            var allScores = await _context.Scores.ToListAsync();
+            return allScores.OrderBy(e => e.Highscore).Take(10).ToList();
         }
+
+        [HttpGet("GetScoreBoardEntry/{id}")]
+        public async Task<ActionResult<Score>> GetScoreBoardEntry(int id) {
+            if (_context.Scores == null) {
+                return NotFound();
+            }
+            var allScores = await _context.Scores.ToListAsync();
+            return allScores.OrderBy(e => e.Highscore).Reverse().Take(10).ToArray()[id];
+        }
+
+
+
+
 
         [HttpPost("NewScore")]
-        public async Task<ActionResult<Score>> PostScore( Score score)
-        {
-          if (_context.Scores == null)
-          {
-              return Problem("Entity set 'ScoreContext.Scores'  is null.");
-          }
+        public async Task<ActionResult<Score>> PostScore(Score score) {
+            if (_context.Scores == null) {
+                return Problem("Entity set 'ScoreContext.Scores'  is null.");
+            }
             _context.Scores.Add(score);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetScore", new { id = score.Id }, score);
         }
 
+        [HttpPost("NewScoreForm")]
+        public async Task<ActionResult<Score>> PostScoreForm([FromForm] Score score) {
+            Console.WriteLine(score.Highscore);
+            if (_context.Scores == null) {
+                return Problem("Entity set 'ScoreContext.Scores'  is null.");
+            }
+            _context.Scores.Add(score);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetScore", new { id = score.Id }, score);
+        }
+
+        
+
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteScore(int id)
-        {
-            if (_context.Scores == null)
-            {
+        public async Task<IActionResult> DeleteScore(int id) {
+            if (_context.Scores == null) {
                 return NotFound();
             }
             var score = await _context.Scores.FindAsync(id);
-            if (score == null)
-            {
+            if (score == null) {
                 return NotFound();
             }
 
@@ -120,8 +105,7 @@ namespace API.Controllers
             return NoContent();
         }
 
-        private bool ScoreExists(int id)
-        {
+        private bool ScoreExists(int id) {
             return (_context.Scores?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
