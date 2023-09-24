bd4994a24c59c7c561f1d9ba0ed6d06e8d5fda52
Justwouter <wouter2312@gmail.com>
2023-09-24 17:00:51 +0200

    Added swagger darkmode & additional documentation

--- API/src/Program.cs
+++ API/src/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 
 using API.Data;
 
@@ -8,22 +11,36 @@ builder.Services.AddDbContext<ScoreContext>(options =>
   options.UseSqlite(builder.Configuration.GetConnectionString("Scoreboard")));
 
 
-
-
-
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+  // add OpenAPI info to the swagger page
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "TetrisV2 Official API", Version = "v0.69", Description = "Scoreboard API for [TetrisV2](https://github.com/Justwouter/Tetrisv2)" });
+
+
+  // Allow XML comments in controller reflected on the swagger page
+  var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+  c.IncludeXmlComments(xmlPath);
+
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-    
+
 }
+
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c => c.InjectStylesheet("/css/SwaggerDark.css"));
+
+// Import css file from the csproj embed
+app.MapGet("/css/SwaggerDark.css", () => {
+  var assembly = Assembly.GetExecutingAssembly();
+  return Results.Stream(assembly.GetManifestResourceStream("API.Assets.SwaggerDark.css")!, "text/css");
+}).ExcludeFromDescription();
 
 // app.UseHttpsRedirection();
 
