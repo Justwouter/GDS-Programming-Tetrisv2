d913b2b151694ca58069e5247ed713c0152409d7
Justwouter <wouter2312@gmail.com>
2023-09-24 16:10:03 +0200

    Added a -p switch to dbuild

--- API/dbuild.sh
+++ API/dbuild.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
+push_flag_present=false
+
+while getopts ":p" opt; do
+    case "$opt" in
+        p)
+            push_flag_present=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+
 # Build locally
 docker build --tag tetrisv2-api .
 
-# Build for remote & push to server
-docker build --tag docker.swijnenburg.cc/tetrisv2-api .
-docker push docker.swijnenburg.cc/tetrisv2-api
+if [ "$push_flag_present" = true ]; then
+    # Build for remote & push to server
+    docker build --tag docker.swijnenburg.cc/tetrisv2-api .
+    docker push docker.swijnenburg.cc/tetrisv2-api
+fi
 
 # Remove intermediate/untagged images
 docker image prune -f
