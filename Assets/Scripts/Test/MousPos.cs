0fbc946820715c9a5c243427c4f2ca5ae4e10beb
Justwouter <wouter2312@gmail.com>
2023-09-25 17:14:59 +0200

    Support for mouse movement, Beta touch support

---
+++ Assets/Scripts/Test/MousPos.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MousPos : MonoBehaviour {
+    Vector3 _mousPos;
+    Vector3 _worldPos;
+    public GameObject Block;
+    public bool MouseHold = false;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        _mousPos = Mouse.current.position.ReadValue();
+        _mousPos.z = Camera.main.nearClipPlane;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(_mousPos);
+        if (worldPos != _worldPos) {
+            _worldPos = worldPos;
+            
+            
+        }
+
+        if(MouseHold){
+            Block.transform.position = new Vector2(_worldPos.x, Block.transform.position.y);
+        }
+
+
+    }
+
+    void OnMouseHold(InputValue inputValue) {
+        Debug.Log("Ich werk");
+        
+        if (inputValue.isPressed) // the key has been pressed
+        {
+            MouseHold = true;
+        }
+        else //the key has been released
+        {
+            MouseHold = false;
+        }
+    }
+}
