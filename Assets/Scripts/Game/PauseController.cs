3ad30d01ac0aba34805986be91f09eb8060a32dc
Justwouter <wouter2312@gmail.com>
2023-10-30 08:50:30 +0100

    Added PauseController

---
+++ Assets/Scripts/Game/PauseController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseController : MonoBehaviour {
+    // Start is called before the first frame update
+    public bool IsPaused = false;
+    private bool _isPauseMenuShown = false;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            IsPaused = !IsPaused;
+            // Time.timeScale = IsPaused ? 0 : 1;
+
+            if (IsPaused) {
+                DisableInput();
+                Time.timeScale = 0;
+            }
+            else {
+                EnableInput();
+                Time.timeScale = 1;
+            }
+        }
+
+
+
+
+    }
+
+    private void DisableInput() {
+        Spawnable[] spawnables = FindObjectsOfType<Spawnable>();
+        foreach (Spawnable item in spawnables) {
+            if (item.isActiveAndEnabled) {
+                Debug.Log("Disabled input for: " + item.name);
+                item.GetComponent<PlayerInput>().enabled = false;
+                item.GetComponent<PlayerInput>().actions.ToList().ForEach(a => a.Disable());
+                item.GetComponent<PlayerInput>().DeactivateInput();
+
+            }
+        }
+    }
+    private void EnableInput() {
+        Spawnable[] spawnables = FindObjectsOfType<Spawnable>();
+        foreach (Spawnable item in spawnables) {
+            if (item.isActiveAndEnabled) {
+                item.GetComponent<PlayerInput>().enabled = true;
+                item.GetComponent<PlayerInput>().actions.ToList().ForEach(a => a.Enable());
+                item.GetComponent<PlayerInput>().ActivateInput();
+
+            }
+        }
+    }
+}
